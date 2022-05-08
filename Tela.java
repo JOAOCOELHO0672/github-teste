7abae989bb55d1979befd46d243ea4e1fce6a34f
Joao Coelho <jcoelho2106@gmail.com>
2022-05-08 18:01:05 -0300

    segundo commit

--- Tela.java
+++ Tela.java
@@ -1,5 +1,18 @@
 package adivinhacao.palavras;
 
 public class Tela {
+	char c;
+	int quantLetrasAcertadas = 0;
+	public void imprimirPalavra(String palavra, int quantLetrasAcertadas){ 
+		int n = palavra.length();
+		for (int i = 0; i < n ; i++) {
+			c = palavra.charAt(i);
+			for (int j = 0; j < quantLetrasAcertadas; j++) {
+				
+			}
+			System.out.println(c); 
+		
+		}
+	}
 
 }
--- Teste.java
+++ Teste.java
@@ -4,31 +4,88 @@ import java.util.Scanner;
 
 public class Teste {
 	
+
 	public static void main(String[] args) {
-		
-		//System.out.println(char o);
-		char c;
-		String s = "ESCOLA";
-		int n = s.length();
+		char letraAtualPalavra;
+		String palavra = "ESCOLA";
+		System.out.println();
+		int quantLetrasPalavra = palavra.length();
+		int quantLetrasAcertadas = 0;
+		char[] letrasAcertadas = new char[quantLetrasPalavra];
 		
 		Scanner scan = new Scanner(System.in);
+
+//		letrasAcertadas[0] = palavra.charAt(0);
+//		letrasAcertadas[2] = palavra.charAt(2);
+//		letrasAcertadas[4] = palavra.charAt(4);
 		
-		System.out.println("Digite uma letra: ");
-		String letra = scan.next();
-		char l = letra.charAt(0); 
+		while (quantLetrasAcertadas <= quantLetrasPalavra) {
+			
+			for (int i = 0; i < quantLetrasPalavra ; i++) {
+				letraAtualPalavra = palavra.charAt(i);
+				if (letrasAcertadas[i] == letraAtualPalavra){
+					System.out.print(letraAtualPalavra);
+				} else {
+					System.out.print("_");
+				}
+	
+						
+
+			}	
+			System.out.println();
+			if (quantLetrasAcertadas == quantLetrasPalavra) {
+				System.out.println("Jogo acabou");
+				break;
+			}
+
+
+				
+			System.out.println("Digite uma letra: ");
+			String letraPalpite = scan.next();
+
+			
+						
+			for (int i = 0; i < quantLetrasPalavra ; i++) {
+				letraAtualPalavra = palavra.charAt(i);
+				if ((letraPalpite.charAt(0) == letraAtualPalavra) && (letraPalpite.charAt(0) 
+						!= (letrasAcertadas[i]))) {
+					letrasAcertadas[i] = letraAtualPalavra;
+					quantLetrasAcertadas++;
+					
+				} 
+				 
+						
+
+			}	
+		// char l = letra.charAt(0); 
+		
+		//tela.imprimirPalavra(palavra, quantLetrasAcertadas);
+			
+		
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+/*		
 		
 		for (int i = 0; i < n ; i++) {
-			c = s.charAt(i);
-			if (c == l) {
-								
-			}
-			System.out.println("a " + (i+1) + "ª letra da palavra " + s 
-					+ " é " + c);
+			c = palavra.charAt(i);
+//			if (c == l) {
+//								
+//			}
+
+
 		}
 		
 		
 		char test = 111;
 		System.out.println(test);
+*/
 	}
 
 }
