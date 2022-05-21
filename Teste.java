be84588f36789a2f3fab5ce3e401c2839bf0ebed
Joao Coelho <jcoelho2106@gmail.com>
2022-05-21 15:47:55 -0300

    commit

--- Teste.java
+++ Teste.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Teste {
 	
-
 	public static void main(String[] args) {
 		char letraAtualPalavra;
 		String palavra = "ESCOLA";
@@ -15,12 +14,10 @@ public class Teste {
 		
 		Scanner scan = new Scanner(System.in);
 
-//		letrasAcertadas[0] = palavra.charAt(0);
-//		letrasAcertadas[2] = palavra.charAt(2);
-//		letrasAcertadas[4] = palavra.charAt(4);
 		
 		while (quantLetrasAcertadas <= quantLetrasPalavra) {
 			
+			// imprime na tela as letras advinhadas da palavra
 			for (int i = 0; i < quantLetrasPalavra ; i++) {
 				letraAtualPalavra = palavra.charAt(i);
 				if (letrasAcertadas[i] == letraAtualPalavra){
@@ -28,64 +25,27 @@ public class Teste {
 				} else {
 					System.out.print("_");
 				}
-	
-						
-
 			}	
+
 			System.out.println();
 			if (quantLetrasAcertadas == quantLetrasPalavra) {
 				System.out.println("Jogo acabou");
 				break;
 			}
-
-
 				
 			System.out.println("Digite uma letra: ");
 			String letraPalpite = scan.next();
-
 			
-						
+			// Verifica se o palpite da letra está correto			
 			for (int i = 0; i < quantLetrasPalavra ; i++) {
 				letraAtualPalavra = palavra.charAt(i);
-				if ((letraPalpite.charAt(0) == letraAtualPalavra) && (letraPalpite.charAt(0) 
+				if ((letraPalpite.charAt(0) == letraAtualPalavra)
+						&& (letraPalpite.charAt(0) 
 						!= (letrasAcertadas[i]))) {
 					letrasAcertadas[i] = letraAtualPalavra;
 					quantLetrasAcertadas++;
-					
 				} 
-				 
-						
-
 			}	
-		// char l = letra.charAt(0); 
-		
-		//tela.imprimirPalavra(palavra, quantLetrasAcertadas);
-			
-		
-		
 		}
-		
-		
-		
-		
-		
-		
-		
-/*		
-		
-		for (int i = 0; i < n ; i++) {
-			c = palavra.charAt(i);
-//			if (c == l) {
-//								
-//			}
-
-
-		}
-		
-		
-		char test = 111;
-		System.out.println(test);
-*/
 	}
-
 }
